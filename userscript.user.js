7628196da5e939937499a83b472c4349bfd1ba4f
Camper <96813893+Camper1412@users.noreply.github.com>
2026-06-21 21:53:58 +0800

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -185,6 +185,8 @@
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @require      https://github.com/Camper1412/HIVE/raw/refs/heads/main/linkhive.user.js
+// @downloadURL  https://github.com/Camper1412/HIVE/raw/refs/heads/main/linkhive.user.js
+// @updateURL    https://github.com/Camper1412/HIVE/raw/refs/heads/main/linkhive.user.js
 // @connect      linkhivepreet.vercel.app
 // @run-at       document-idle
 // ==/UserScript==
